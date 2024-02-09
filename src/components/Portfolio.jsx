1f0128d5bc9c671ce75ffc1ff7ac7b8b109faf96
Sofie Christensen <sofiec510@gmail.com>
2024-02-09 14:31:47 -0500

    map portfolio data

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { projects } from "../data";
 
 function Portfolio() {
   return (
     <>
-      <div>Portfolio</div>
+      <div className="portfolio-header">
+        <h1 className="header">Apps I've Built</h1>
+        <p className="portfolio-description">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+          facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
+          fuga dolore.
+        </p>
+      </div>
+      <div className="projects-section">
+        {projects.map((project) => (
+          <a
+            href={project.link}
+            key={project.image}
+            className="project-individual"
+          >
+            <h1 className="project-title">{project.title}</h1>
+            <h2 className="subtitle">{project.subtitle}</h2>
+            <p>{project.description}</p>
+            <img
+              alt="image of project"
+              className="project-image"
+              src={project.image}
+            />
+          </a>
+        ))}
+      </div>
     </>
   );
 }
