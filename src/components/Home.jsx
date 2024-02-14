2f20a553ef348f6223403e312309258d11fa04f9
Sofie Christensen <sofiec510@gmail.com>
2024-02-14 14:33:55 -0500

    fonts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,16 @@ import React from "react";
 function Home() {
   return (
     <>
-      <div>Homepage</div>
+      <div className="homepage-content">
+        <div className="homepage-block">
+          <img className="headshot" src="src/assets/schristensenheadshot.jpg" />
+        </div>
+        <div className="homepage-block">
+          <h1>Sofie Christensen</h1>
+          <h2>Software Engineer</h2>
+          <h3>Researcher, Musician</h3>
+        </div>
+      </div>
     </>
   );
 }
