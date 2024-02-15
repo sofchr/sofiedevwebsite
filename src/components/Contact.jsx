fb1b269f088c667ef1e34e0e69f2166f99ef77a9
Sofie Christensen <sofiec510@gmail.com>
2024-02-15 13:27:59 -0500

    navbar and contact page styling basics

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,15 +22,20 @@ function Contact() {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Your Name:</label>
-      <input type="text" name="from_name" />
-      <label>Your Email Address:</label>
-      <input type="email" name="user_email" />
-      <label>Message:</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="contact-content">
+      <div className="contact-section">
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <label>Your Name:</label>
+          <input type="text" name="from_name" />
+          <label>Your Email Address:</label>
+          <input type="email" name="user_email" />
+          <label>Message:</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+      <div className="contact-section">LinkedIn and GitHub</div>
+    </div>
   );
 }
 
